e09e7e80aced86322526c7bbd3bad261cf09fa11
aditayadav <aditayadav8@gmail.com>
2024-12-26 23:56:03 +0545

    setup env and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import appConfig from "./config";
+import { AppDataSource } from "ormconfig";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+  .setTitle("Blog App")
+  .setDescription("Blog app API description")
+  .setVersion("1.0")
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  // database initialize
+  await AppDataSource.initialize();
+
+  //server setup
+  await app.listen(appConfig.PORT);
 }
 bootstrap();
